d8134a4b16a7ac1a04664b2f516b88ac240e77c9
longo2002 <lombardo.fra2002@gmail.com>
2023-02-07 15:54:34 +0100

    attempt 2

--- js/app.js
+++ js/app.js
@@ -4,23 +4,15 @@ const oddNums = []
 
 // RECUPERO IL NUMERO
 let n = parseInt(prompt("Inserisci un numero"))
-// TRASFORMO IL NUMERO IN INTERO
-let nInt= parseInt(n)
 
 // MODULO PER CAPIRE SE E' PARI
 let mod2 = ( n % 2 === 0 )
 
 // FILTRO IL NUMERO TRA PARI O DISPARI
-
-while( n = mod2 ){
-
+while( isNaN(n) || (n = mod2) ){
+    n = parseInt(prompt("Inserisci un numero"))
 }
-
-if (n = mod2){
-    console.log("Pari")
-} else {
-    oddNums.push(nInt)
-}
+oddNums.push(n)
 
 // STAMPO L'ARRAY
 console.log(oddNums)
